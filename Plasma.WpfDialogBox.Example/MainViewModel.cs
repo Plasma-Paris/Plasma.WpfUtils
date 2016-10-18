10527d79c4d926d029ca47af0176b3a6325756ea
Xavier Guillemot <xavier@plasma.paris>
2016-10-18 18:52:51 +0200

    feat: Add a simple example project

---
+++ Plasma.WpfDialogBox.Example/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Plasma.WpfDialogBox.ViewModels;
+using Plasma.WpfFrameWork;
+
+namespace Plasma.WpfDialogBox.Example
+{
+    public class MainViewModel : ViewModelBase
+    {
+        MessageBoxService _Service;
+        public MainViewModel()
+        {
+            var defaultMessageBoxViewModel = new GenericMessageBoxViewModel();
+            
+            _Service = new MessageBoxService(defaultMessageBoxViewModel);
+
+            _Service.OnShowRequestStarted += (s, e) => CurrentMessageBox = e.ViewModel;
+            _Service.OnHideRequestEnded += (s, e) => CurrentMessageBox = null;
+
+            YesNoMessageBoxCommand = new RelayCommand(YesNoMessageBox);
+        }
+
+        private MessageBoxContainerViewModel _CurrentMessageBox;
+        public MessageBoxContainerViewModel CurrentMessageBox
+        {
+            get
+            {
+                return _CurrentMessageBox;
+            }
+            set
+            {
+                Set(() => CurrentMessageBox, ref _CurrentMessageBox, value);
+            }
+        }
+
+        public RelayCommand YesNoMessageBoxCommand { get; private set; }
+
+        async void YesNoMessageBox()
+        {
+            var result = await _Service.ShowMessage("This is the content of the message box","This is the title",System.Windows.MessageBoxButton.YesNo);
+            if (result == System.Windows.MessageBoxResult.Yes)
+                await _Service.ShowMessage("User chosse Yes !");
+            if (result == System.Windows.MessageBoxResult.No)
+                await _Service.ShowMessage("User chosse No !");
+        }
+    }
+}
